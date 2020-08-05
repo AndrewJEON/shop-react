416bcaacd38dee9e9e84b6c717bd5768a5272266
Andrew JEON <andrew.jeon@funda.kr>
2020-08-05 23:52:23 +0900

    Updates

---
+++ precache-manifest.97d5d1f1158321739e43779e2aa6ca46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ffd2afb0ae36ebff96a06a56c503cb",
+    "url": "/shop-react/index.html"
+  },
+  {
+    "revision": "2cee100356170c264ad4",
+    "url": "/shop-react/static/css/main.8fe1c7d6.chunk.css"
+  },
+  {
+    "revision": "a1bf6e271a28adae0740",
+    "url": "/shop-react/static/js/2.56145d8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shop-react/static/js/2.56145d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cee100356170c264ad4",
+    "url": "/shop-react/static/js/main.f9949a20.chunk.js"
+  },
+  {
+    "revision": "823087175de54c586158",
+    "url": "/shop-react/static/js/runtime-main.5773c02f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shop-react/static/media/crown.ed28accb.svg"
+  }
+]);
